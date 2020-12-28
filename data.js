1ff6a94f5dfe4e03bf5f93928e7ed896fc11a7c0
Tobias Rosenberg <mail@trosenberg.com>
2020-12-28 19:53:15 +0000

    add post function

--- data.js
+++ data.js
@@ -8,4 +8,24 @@ const testSheet2Api = () => {
         })
 }
 
+const addGS = () => {
+    const url = "https://script.google.com/macros/s/AKfycbwaw1U6tBqjjVZeZX6BaqpQm-naQJ3ZSHWgmSgR0AcQjBLAJg7yQlAQ/exec";
+
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"some name"}) // body data type must match "Content-Type" header
+      });
+
+}
+
 document.getElementById("btn").addEventListener("click", testSheet2Api);
+
+document.getElementById("btn2").addEventListener("click", addGS);
